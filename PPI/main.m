04fcb3ad2c389cccb9fcf98030eeccdfbe55f6e0
linxichen <linxichen88@gmail.com>
2015-05-31 13:48:45 -0400

    Fixed various mistakes in finding Error. Still very big.

--- PPI/main.m
+++ PPI/main.m
@@ -163,33 +163,34 @@ Aee = linspace(0.8,1.2,nA_ee);
 Nee = linspace(0.7,0.999,nN_ee);
 EEerror_c = 999999*ones(nA_ee*nK_ee*nN_ee,1);
 EEerror_v = 999999*ones(nA_ee*nK_ee*nN_ee,1);
+cc = zeros(nA_ee*nK_ee*nN_ee,1);
 
 for index = 1:nA_ee*nK_ee*nN_ee
-    [i_a,i_k,i_n] = ind2sub([nA,nK,nN],index);
-    A = Aee(i_a);
+    [i_a,i_k,i_n] = ind2sub([nA_ee,nK_ee,nN_ee],index);
+    a = Aee(i_a);
     k = Kee(i_k);
     n = Nee(i_n);
-    state = [A,k,n,A*k^aalpha*n^(1-aalpha) + (1-ddelta)*k + z*(1-n),xxi*(1-n)^(1-eeta)];
+    state = [a,k,n,a*k^aalpha*n^(1-aalpha) + (1-ddelta)*k + z*(1-n),xxi*(1-n)^(1-eeta)];
     
     x0 = [k,n];
     lb = [500,(1-x)*n]; ub = [3000,0.999];
     [temp_policy,~,~] = nested_obj(state,param,pphi,epsi_nodes,weight_nodes,n_nodes,x0,lb,ub,options);
     kplus = temp_policy(1); nplus = temp_policy(2);
     v = ((nplus - (1-x)*n)/state(5))^(1/eeta);
-    c = state(4) - kplus - kkappa*v;
+    cc(index) = state(4) - kplus - kkappa*v;
     
     % Find expected mf and mh and implied consumption
     Emf = 0; Emh = 0;
     for i_node = 1:length(weight_nodes)
-        Aplus = exp(rrho_A*log(A) + ssigma_A*epsi_nodes(i_node));
+        Aplus = exp(rrho_A*log(a) + ssigma_A*epsi_nodes(i_node));
         stateplus = [Aplus,kplus,nplus,Aplus*kplus^aalpha*nplus^(1-aalpha) + (1-ddelta)*kplus + z*(1-nplus),xxi*(1-nplus)^(1-eeta)];
 
         x0 = [kplus,nplus];
         lb = [500,(1-x)*nplus]; ub = [3000,0.999];
         [temp_policy,~,~] = nested_obj(stateplus,param,pphi,epsi_nodes,weight_nodes,n_nodes,x0,lb,ub,options);
         kplusplus = temp_policy(1); nplusplus = temp_policy(2);
-        vplus = ((nplusplus - (1-x)*n)/state(5))^(1/eeta);
-        cplus = state(4) - kplusplus - kkappa*vplus;
+        vplus = ((nplusplus - (1-x)*nplus)/stateplus(5))^(1/eeta);
+        cplus = stateplus(4) - kplusplus - kkappa*vplus;
         tthetaplus = vplus/(1-nplus); yplus = Aplus*kplus^aalpha*nplus^(1-aalpha);
         
         Emh = Emh + weight_nodes(i_node)*((1-ddelta+aalpha*yplus/kplus)/cplus);
